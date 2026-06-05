6fc70145f717953315707a86750513e9204f682d
Liia.Practicum <LK12345678999@yandex.ru>
2026-06-05 18:58:01 +0300

    Сделала базовую версию Counter

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var counterButton: UIButton!
+    
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterButton.configuration?.baseForegroundColor = .green
+        
+        counterLabel.text = "Значение счетчика: 0"
     }
 
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+    counter += 1
+    counterLabel.text = "Значение счетчика: \(counter)"
+    }
+    
 }
 
